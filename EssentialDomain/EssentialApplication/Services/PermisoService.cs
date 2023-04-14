373524f9cdb04dcaef4fbb9a206153bbf88989b8
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-14 15:55:11 -0600

    se crean los servicios de roles, permisos y permisos_rol

---
+++ EssentialDomain/EssentialApplication/Services/PermisoService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EssentialApplication.dtos;
+using EssentialApplication.Interfaces;
+using EssentialCore.Entities;
+using EssentialCore.Interfaces.Repositories;
+using EssentialCore.Services;
+
+namespace EssentialApplication.Services
+{
+    public class PermisoService : BaseService<IPermisoRepository, Permiso, long, PermisoDto>, IPermisoService
+    {
+        public PermisoService(IPermisoRepository repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
---
+++ EssentialDomain/EssentialApplication/Services/PermisosRolService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EssentialApplication.dtos;
+using EssentialApplication.Interfaces;
+using EssentialCore.Entities;
+using EssentialCore.Interfaces.Repositories;
+using EssentialCore.Services;
+
+namespace EssentialApplication.Services
+{
+    public class PermisosRolService : BaseService<IPermisosRolRepository, PermisosRol, long, PermisosRolDto>, IPermisosRolService
+    {
+        public PermisosRolService(IPermisosRolRepository repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
