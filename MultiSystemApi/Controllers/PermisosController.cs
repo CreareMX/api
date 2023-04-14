433ff8357ac3b5435b95e3ee2ea61247de109b4f
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-14 15:59:29 -0600

    se crean los controladores de API de roles, permisos y permisos_roles

---
+++ MultiSystemApi/Controllers/PermisosController.cs
@@ -0,0 +1,63 @@
+﻿using EssentialApplication.dtos;
+using EssentialApplication.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiSystemApi.ContPermisolers
+{
+    [Route("api/[contPermisoler]")]
+    [ApiController]
+    public class PermisosController : ControllerBase
+    {
+        private IPermisoService Service { get; set; }
+        public PermisosController(IPermisoService service)
+        {
+            Service = service;
+        }
+
+        [HttpGet("id/{id}")]
+        public PermisoDto GetById(long id) => Service.GetById(id);
+        [HttpGet("all")]
+        public List<PermisoDto> GetAll() => Service.GetAll().ToList();
+        [HttpPost("{idUser}")]
+        public IActionResult Create(PermisoDto dto, long idUser)
+        {
+            try
+            {
+                var entity = Service.Create(dto, idUser);
+                if (entity == null)
+                    return NoContent();
+                return Ok(entity);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{idUser}")]
+        public IActionResult Update(PermisoDto dto, long idUser)
+        {
+            try
+            {
+                Service.Update(dto, idUser);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{idUser}")]
+        public IActionResult Delete(PermisoDto dto, long idUser)
+        {
+            try
+            {
+                Service.Delete(dto.Id, idUser);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MultiSystemApi/Controllers/RolesController.cs
@@ -0,0 +1,63 @@
+﻿using EssentialApplication.dtos;
+using EssentialApplication.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private IRolService Service { get; set; }
+        public RolesController(IRolService service)
+        {
+            Service = service;
+        }
+
+        [HttpGet("id/{id}")]
+        public RolDto GetById(long id) => Service.GetById(id);
+        [HttpGet("all")]
+        public List<RolDto> GetAll() => Service.GetAll().ToList();
+        [HttpPost("{idUser}")]
+        public IActionResult Create(RolDto dto, long idUser)
+        {
+            try
+            {
+                var entity = Service.Create(dto, idUser);
+                if (entity == null)
+                    return NoContent();
+                return Ok(entity);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{idUser}")]
+        public IActionResult Update(RolDto dto, long idUser)
+        {
+            try
+            {
+                Service.Update(dto, idUser);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{idUser}")]
+        public IActionResult Delete(RolDto dto, long idUser)
+        {
+            try
+            {
+                Service.Delete(dto.Id, idUser);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
