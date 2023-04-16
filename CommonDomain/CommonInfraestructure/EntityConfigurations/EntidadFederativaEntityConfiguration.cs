2bd7b5b9820d2c10a0acfc6d63b7f64ca7f56a23
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-16 12:17:42 -0600

    se crean entidad y mapeo para el objeto de entidad federativa

---
+++ CommonDomain/CommonInfraestructure/EntityConfigurations/EntidadFederativaEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CommonCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommonInfraestructure.EntityConfigurations
+{
+    public class EntidadFederativaEntityConfiguration : IEntityTypeConfiguration<EntidadFederativa>
+    {
+        public void Configure(EntityTypeBuilder<EntidadFederativa> builder)
+        {
+            builder.ToTable("EntidadFederativas");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Id).HasColumnName("Id").IsRequired();
+            builder.Property(p => p.Nombre).HasColumnName("Nombre").IsRequired();
+        }
+    }
+}
