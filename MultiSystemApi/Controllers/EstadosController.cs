70ec853abacf898d608561abd48295f7263ea80c
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-21 23:05:21 -0600

    documentacion del controlador de estados

--- MultiSystemApi/Controllers/EstadosController.cs
+++ MultiSystemApi/Controllers/EstadosController.cs
@@ -5,20 +5,41 @@ using EssentialCore.Shared;
 
 namespace MultiSystemApi.Controllers
 {
+    /// <summary>
+    /// Controlador del API de estados
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class EstadosController : ControllerBase
     {
         private IEstadoService Service { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="service">Servicio de estados</param>
         public EstadosController(IEstadoService service)
         {
             Service = service;
         }
-
+        /// <summary>
+        /// Obtiene un estado por medio de su ID
+        /// </summary>
+        /// <param name="id">Identificador único del estado</param>
+        /// <returns>Estados</returns>
         [HttpGet("id/{id}")]
         public EstadoDto GetById(long id) => Service.GetById(id);
+        /// <summary>
+        /// Obtiene toda la lista de estados
+        /// </summary>
+        /// <returns>Estados</returns>
         [HttpGet("all")]
         public List<EstadoDto> GetAll() => Service.GetAll().ToList();
+        /// <summary>
+        /// Crea un nuevo estado
+        /// </summary>
+        /// <param name="dto">Datos del estado</param>
+        /// <param name="idUser">ID del usuario que crea el estado</param>
+        /// <returns>Estado</returns>
         [HttpPost("{idUser}")]
         public IActionResult Create(EstadoDto dto, long idUser)
         {
@@ -34,6 +55,12 @@ namespace MultiSystemApi.Controllers
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Actualiza un estado
+        /// </summary>
+        /// <param name="dto">Datos del estado</param>
+        /// <param name="idUser">ID del usuario que actualiza el estado</param>
+        /// <returns>Success</returns>
         [HttpPut("{idUser}")]
         public IActionResult Update(EstadoDto dto, long idUser)
         {
@@ -47,6 +74,12 @@ namespace MultiSystemApi.Controllers
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Desactiva un estado existente
+        /// </summary>
+        /// <param name="dto">Datos del estado (se requiere únicamente el ID)</param>
+        /// <param name="idUser">ID del usuario que desactiva el estado</param>
+        /// <returns>success</returns>
         [HttpDelete("{idUser}")]
         public IActionResult Delete(EstadoDto dto, long idUser)
         {
