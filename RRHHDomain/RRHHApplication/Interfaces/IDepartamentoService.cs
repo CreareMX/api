61b8c4ee9b34dc84bc872b96c61cb2f41c46aff1
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-22 16:12:40 -0600

    se crea API de despartamentos

---
+++ RRHHDomain/RRHHApplication/Interfaces/IDepartamentoService.cs
@@ -0,0 +1,11 @@
+﻿using EssentialCore.Interfaces.Service;
+using RRHHApplication.Dtos;
+using RRHHCore.Entities;
+using RRHHCore.Interfaces.Repositories;
+
+namespace RRHHApplication.Interfaces
+{
+    public interface IDepartamentoService : IService<IDepartamentoRepository, Departamento, long, DepartamentoDto>
+    {
+    }
+}
---
+++ RRHHDomain/RRHHInfraestructure/EntityConfigurations/DepartamentoEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RRHHCore.Entities;
+
+namespace RRHHInfraestructure.EntityConfigurations
+{
+    public class DepartamentoEntityConfiguration : IEntityTypeConfiguration<Departamento>
+    {
+        public void Configure(EntityTypeBuilder<Departamento> builder)
+        {
+            builder.ToTable("departamentos");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Id).HasColumnName("id").IsRequired();
+            builder.Property(p => p.Nombre).HasColumnName("nombre").IsRequired();
+            builder.Property(p => p.IdResponsable).HasColumnName("id_responsable").IsRequired();
+            builder.Property(p => p.Activo).HasColumnName("activo").IsRequired();
+            builder.Property(p => p.FechaCreacion).HasColumnName("fecha_creacion").IsRequired();
+            builder.Property(p => p.UsuarioCreaId).HasColumnName("id_usuario_creacion").IsRequired();
+            builder.Property(p => p.FechaUltimaActualizacion).HasColumnName("fecha_ultima_actualizacion").IsRequired(false);
+            builder.Property(p => p.UsuarioActualizaId).HasColumnName("id_usuario_ultima_actualizacion").IsRequired(false);
+
+            builder.HasOne(p => p.UsuarioCrea)
+                .WithMany()
+                .HasForeignKey(p => p.UsuarioCreaId);
+
+            builder.HasOne(p => p.UsuarioActualiza)
+                .WithMany()
+                .HasForeignKey(p => p.UsuarioActualizaId);
+
+            builder.HasOne(p => p.Responsable)
+                .WithMany()
+                .HasForeignKey(p => p.IdResponsable);
+        }
+    }
+}
