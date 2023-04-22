dee1cccb359d11107440e9a0cd4809e2a4ce9369
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-21 18:54:31 -0600

    documentacion del controlador de cuentas por cobrar

--- MultiSystemApi/Controllers/Contabilidad/CuentasPorCobrarController.cs
+++ MultiSystemApi/Controllers/Contabilidad/CuentasPorCobrarController.cs
@@ -5,20 +5,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MultiSystemApi.Controllers.Contabilidad
 {
+    /// <summary>
+    /// Controlador del API de cuentas por cobrar
+    /// </summary>
     [Route("api/contabilidad/[controller]")]
     [ApiController]
     public class CuentasPorCobrarController : ControllerBase
     {
         private ICuentaPorCobrarService Service { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="service">Servicio de cuentas por cobrar</param>
         public CuentasPorCobrarController(ICuentaPorCobrarService service)
         {
             Service = service;
         }
-
+        /// <summary>
+        /// Obtiene una cuenta por cobrar por medio de su ID
+        /// </summary>
+        /// <param name="id">Identificador único de la cuenta por cobrar</param>
+        /// <returns>Cuenta por cobrar</returns>
         [HttpGet("id/{id}")]
         public CuentaPorCobrarDto GetById(long id) => Service.GetById(id);
+        /// <summary>
+        /// Obtiene toda la lista de cuentas por cobrar
+        /// </summary>
+        /// <returns>Cuentas por cobrar</returns>
         [HttpGet("all")]
         public List<CuentaPorCobrarDto> GetAll() => Service.GetAll().ToList();
+        /// <summary>
+        /// Crea una nueva cuenta por cobrar
+        /// </summary>
+        /// <param name="dto">Datos de la cuenta por cobrar</param>
+        /// <param name="idUser">ID del usuario que crea la cuenta por cobrar</param>
+        /// <returns>Cuenta por cobrar</returns>
         [HttpPost("{idUser}")]
         public IActionResult Create(CuentaPorCobrarDto dto, long idUser)
         {
@@ -34,6 +55,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Actualiza una cuenta por cobrar
+        /// </summary>
+        /// <param name="dto">Datos de la cuenta por cobrar</param>
+        /// <param name="idUser">ID del usuario que actualiza la cuenta por cobrar</param>
+        /// <returns>Success</returns>
         [HttpPut("{idUser}")]
         public IActionResult Update(CuentaPorCobrarDto dto, long idUser)
         {
@@ -47,6 +74,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Desactiva una cuenta por cobrar existente
+        /// </summary>
+        /// <param name="dto">Datos de la cuenta por cobrar (se requiere únicamente el ID)</param>
+        /// <param name="idUser">ID del usuario que desactiva la cuenta por cobrar</param>
+        /// <returns>success</returns>
         [HttpDelete("{idUser}")]
         public IActionResult Delete(CuentaPorCobrarDto dto, long idUser)
         {
