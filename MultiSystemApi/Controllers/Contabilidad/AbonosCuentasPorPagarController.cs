034016bcfd10e105f63a40ec7417dc9a28046992
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-21 18:50:48 -0600

    documentacion del controlador de abonos a cuentas por pagar

--- MultiSystemApi/Controllers/Contabilidad/AbonosCuentasPorPagarController.cs
+++ MultiSystemApi/Controllers/Contabilidad/AbonosCuentasPorPagarController.cs
@@ -5,20 +5,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MultiSystemApi.Controllers.Contabilidad
 {
+    /// <summary>
+    /// Controlador del API de abonos a cuentas por pagar
+    /// </summary>
     [Route("api/contabilidad/[controller]")]
     [ApiController]
     public class AbonosCuentasPorPagarController : ControllerBase
     {
         private IAbonoCuentaPorPagarService Service { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="service">Servicio de abonos a cuentas por pagar</param>
         public AbonosCuentasPorPagarController(IAbonoCuentaPorPagarService service)
         {
             Service = service;
         }
-
+        /// <summary>
+        /// Obtiene un abono a cuenta por pagar por medio de su ID
+        /// </summary>
+        /// <param name="id">Identificador único de abono a cuenta por pagar</param>
+        /// <returns>Abono a cuenta por pagar</returns>
         [HttpGet("id/{id}")]
         public AbonoCuentaPorPagarDto GetById(long id) => Service.GetById(id);
+        /// <summary>
+        /// Obtiene toda la lista de abonos a cuentas por pagar
+        /// </summary>
+        /// <returns>Abonos a cuentas por pagar</returns>
         [HttpGet("all")]
         public List<AbonoCuentaPorPagarDto> GetAll() => Service.GetAll().ToList();
+        /// <summary>
+        /// Crea un nuevo abono a cuenta por pagar
+        /// </summary>
+        /// <param name="dto">Datos del abono a cuenta por pagar</param>
+        /// <param name="idUser">ID del usuario que crea el abono a cuenta por pagar</param>
+        /// <returns>Abono a cuenta por pagar</returns>
         [HttpPost("{idUser}")]
         public IActionResult Create(AbonoCuentaPorPagarDto dto, long idUser)
         {
@@ -34,6 +55,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Actualiza un abono a cuenta por pagar
+        /// </summary>
+        /// <param name="dto">Datos del abono a cuenta por pagar</param>
+        /// <param name="idUser">ID del usuario que actualiza el abono a cuenta por pagar</param>
+        /// <returns>Success</returns>
         [HttpPut("{idUser}")]
         public IActionResult Update(AbonoCuentaPorPagarDto dto, long idUser)
         {
@@ -47,6 +74,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Desactiva un abono a cuenta por pagar existente
+        /// </summary>
+        /// <param name="dto">Datos del abono a cuenta por pagar (se requiere únicamente el ID)</param>
+        /// <param name="idUser">ID del usuario que desactiva el abono a cuenta por pagar</param>
+        /// <returns>success</returns>
         [HttpDelete("{idUser}")]
         public IActionResult Delete(AbonoCuentaPorPagarDto dto, long idUser)
         {
