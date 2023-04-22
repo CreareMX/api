899ef30cf1e6affda406706f34c400cfdd923fe8
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-21 19:00:26 -0600

    documentacion del controlador de datos fiscales

--- MultiSystemApi/Controllers/Contabilidad/DatosFiscalesController.cs
+++ MultiSystemApi/Controllers/Contabilidad/DatosFiscalesController.cs
@@ -5,20 +5,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MultiSystemApi.Controllers.Contabilidad
 {
+    /// <summary>
+    /// Controlador del API de datos fiscales
+    /// </summary>
     [Route("api/contabilidad/[controller]")]
     [ApiController]
     public class DatosFiscalesController : ControllerBase
     {
         private IDatosFiscalesService Service { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="service">Servicio de datos fiscales</param>
         public DatosFiscalesController(IDatosFiscalesService service)
         {
             Service = service;
         }
-
+        /// <summary>
+        /// Obtiene datos fiscales por medio de su ID
+        /// </summary>
+        /// <param name="id">Identificador único de la entidad de datos fiscales</param>
+        /// <returns>Datos fiscales</returns>
         [HttpGet("id/{id}")]
         public DatosFiscalesDto GetById(long id) => Service.GetById(id);
+        /// <summary>
+        /// Obtiene toda la lista de datos fiscales
+        /// </summary>
+        /// <returns>Datos fiscales</returns>
         [HttpGet("all")]
         public List<DatosFiscalesDto> GetAll() => Service.GetAll().ToList();
+        /// <summary>
+        /// Crea una nueva entidad de datos fiscales
+        /// </summary>
+        /// <param name="dto">Datos de la entidad de datos fiscales</param>
+        /// <param name="idUser">ID del usuario que crea entidad de datos fiscales</param>
+        /// <returns>Datos fiscales</returns>
         [HttpPost("{idUser}")]
         public IActionResult Create(DatosFiscalesDto dto, long idUser)
         {
@@ -34,6 +55,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Actualiza una entidad de datos fiscales
+        /// </summary>
+        /// <param name="dto">Datos de la entidad de datos fiscales</param>
+        /// <param name="idUser">ID del usuario que actualiza la entidad de datos fiscales</param>
+        /// <returns>Success</returns>
         [HttpPut("{idUser}")]
         public IActionResult Update(DatosFiscalesDto dto, long idUser)
         {
@@ -47,6 +74,12 @@ namespace MultiSystemApi.Controllers.Contabilidad
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Desactiva una cuenta por cobrar existente
+        /// </summary>
+        /// <param name="dto">Datos de la cuenta por cobrar (se requiere únicamente el ID)</param>
+        /// <param name="idUser">ID del usuario que desactiva la cuenta por cobrar</param>
+        /// <returns>success</returns>
         [HttpDelete("{idUser}")]
         public IActionResult Delete(DatosFiscalesDto dto, long idUser)
         {
