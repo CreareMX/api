bf5c8abee8a879f6bf3cfc093b9195fcb649a144
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-22 10:29:32 -0600

    documentacion de controlador de productos

--- MultiSystemApi/Controllers/ProductosController.cs
+++ MultiSystemApi/Controllers/ProductosController.cs
@@ -5,20 +5,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MultiSystemApi.Controllers
 {
+    /// <summary>
+    /// Controlador del API de productos
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ProductosController : ControllerBase
     {
         private IProductoService Service { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="service">Servicio de productos</param>
         public ProductosController(IProductoService service)
         {
             Service = service;
         }
-
+        /// <summary>
+        /// Obtiene un producto por medio de su ID
+        /// </summary>
+        /// <param name="id">Identificador único del producto</param>
+        /// <returns>Productos</returns>
         [HttpGet("id/{id}")]
         public ProductoDto GetById(long id) => Service.GetById(id);
+        /// <summary>
+        /// Obtiene toda la lista de productos
+        /// </summary>
+        /// <returns>Productos</returns>
         [HttpGet("all")]
         public List<ProductoDto> GetAll() => Service.GetAll().ToList();
+        /// <summary>
+        /// Crea un nuevo producto
+        /// </summary>
+        /// <param name="dto">Datos del producto</param>
+        /// <param name="idUser">ID del usuario que crea el producto</param>
+        /// <returns>Producto</returns>
         [HttpPost("{idUser}")]
         public IActionResult Create(ProductoDto dto, long idUser)
         {
@@ -34,6 +55,12 @@ namespace MultiSystemApi.Controllers
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Actualiza un producto
+        /// </summary>
+        /// <param name="dto">Datos del producto</param>
+        /// <param name="idUser">ID del usuario que actualiza el producto</param>
+        /// <returns>Success</returns>
         [HttpPut("{idUser}")]
         public IActionResult Update(ProductoDto dto, long idUser)
         {
@@ -47,6 +74,12 @@ namespace MultiSystemApi.Controllers
                 return BadRequest(ExceptionHelper.GetFullMessage(ex));
             }
         }
+        /// <summary>
+        /// Desactiva un producto existente
+        /// </summary>
+        /// <param name="dto">Datos del producto (se requiere únicamente el ID)</param>
+        /// <param name="idUser">ID del usuario que desactiva el producto</param>
+        /// <returns>success</returns>
         [HttpDelete("{idUser}")]
         public IActionResult Delete(ProductoDto dto, long idUser)
         {
