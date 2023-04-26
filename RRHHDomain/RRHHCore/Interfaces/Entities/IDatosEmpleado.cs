fd1c4459c2f65bda341a1978d56ded46529613bc
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-25 18:23:16 -0600

    se crea api de datos de empleados

---
+++ RRHHDomain/RRHHCore/Interfaces/Entities/IDatosEmpleado.cs
@@ -0,0 +1,11 @@
+﻿namespace RRHHCore.Interfaces.Entities
+{
+    public interface IDatosEmpleado
+    {
+        decimal Salario { get; set; }
+        DateTime FechaContratacion { get; set; }
+        long IdDepartamento { get; set; }
+        long IdEmpleado { get; set; }
+        long IdPuesto { get; set; }
+    }
+}
