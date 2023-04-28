2ed0c7a82392c393cc47a8c9ddd3503cf28b774b
Wilson Eliseo Alcocer Flores <walcocer@intermexusa.com>
2023-04-27 18:13:01 -0600

    se complementa el API de productos con las propiedades numero de serie y categorias

--- CommonDomain/CommonApplication/Dtos/ProductoDto.cs
+++ CommonDomain/CommonApplication/Dtos/ProductoDto.cs
@@ -8,6 +8,9 @@ namespace CommonApplication.Dtos
         public string Descripcion { get; set; }
         public string CodigoBarras { get; set; }
         public string Codigo { get; set; }
+        public string NumeroSerie { get; set; }
+        public long IdCategoria { get; set; }
+        public CategoriaDto Categoria { get; set; }
         public long? Id { get; set; }
     }
 }
